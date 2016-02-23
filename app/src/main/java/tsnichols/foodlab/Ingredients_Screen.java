1fa6505629e836814860b9b98e6f65c213722d48
TSNichols <talcott.nichols@gmail.com>
2016-02-23 16:05:25 -0500

    Added fragments, and content to those

--- app/src/main/java/tsnichols/foodlab/Ingredients_Screen.java
+++ app/src/main/java/tsnichols/foodlab/Ingredients_Screen.java
@@ -2,12 +2,27 @@ package tsnichols.foodlab;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class Ingredients_Screen extends AppCompatActivity {
+
+    Button New_Ingredient_BTN;
+    Button New_Size_BTN;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.ingredients_screen);
+        addListenerOnButton();
+    }
+
+    public void addListenerOnButton(){
+
+        New_Ingredient_BTN = (Button) findViewById(R.id.btn_new_ingredient);
+        New_Size_BTN = (Button) findViewById(R.id.btn_new_size);
+
+
+
+
     }
 }
